01aed87bb9ff811f81a2aa44e500dac712be57bd
aforembs <andrew.forembski2@mail.dcu.ie>
2022-09-15 17:25:38 +0100

    Starting with 2e TDSE

---
+++ src/tdse2e/include/td2e.hpp
@@ -0,0 +1,12 @@
+#ifndef TD2E_HPP_
+#define TD2E_HPP_
+
+#include <iostream>
+#include <complex>
+#include <vector>
+
+namespace td2e {
+  
+}
+
+#endif // TD2E_HPP_
---
+++ src/tdse2e/td2e.cpp
@@ -0,0 +1,1 @@
+#include "td2e.hpp"
