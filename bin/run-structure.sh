022bb374851a70a81e22f62987df91423857bd7e
aforembs <andrew.forembski2@mail.dcu.ie>
2023-01-17 12:58:48 +0000

    Added structure script and more cmake options

---
+++ bin/run-structure.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+function usage {
+        echo "Usage: $(basename $0) [-he:f:i:]" 2>&1
+        echo '   -h   shows this help message'
+        echo '   -e   the numberof electrons 1 or 2'
+        echo '   -f   path to the .yaml input file'
+        echo '   -i   path to directory of the cfg-<L>.inp files only used if -e 2'
+        exit 1
+}
+
+if [[ ${#} -eq 0 ]]; then
+   usage
+fi
+
+# Define list of arguments expected in the input
+optstring=":he:f:i:"
+
+while getopts ${optstring} arg; do
+  case "${arg}" in
+    h)  usage 
+        ;;
+    e) NUM_E=${OPTARG} ;;
+    f) YAML_FILE=${OPTARG} ;;
+    i) CFG_DIR=${OPTARG} ;;
+
+    ?)
+      echo "Invalid option: -${OPTARG}."
+      echo
+      usage
+      ;;
+  esac
+done
+
+if [[ ${NUM_E} == 1 ]]; then
+  bin/h1e -f ${YAML_FILE}
+  bin/w1e -f ${YAML_FILE}
+  bin/dmx1e -f ${YAML_FILE}
+elif [[ ${NUM_E} == 2 ]]; then
+  bin/h1e -f ${YAML_FILE}
+  bin/w1e -f ${YAML_FILE}
+  bin/dmx1e -f ${YAML_FILE}
+  bin/gen2eidx -f ${YAML_FILE} -i ${CFG_DIR}
+  bin/r12 -f ${YAML_FILE} -i ${CFG_DIR}
+  bin/dmx2e -f ${YAML_FILE} -i ${CFG_DIR}
+  bin/cibasis -f ${YAML_FILE}
+else
+  echo "Invalid number of electrons: ${NUM_E}."
+  echo "Only 1 or 2 supported."
+fi
